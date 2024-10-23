6f11e59562aea084a173ee5377703ef8dfa81bc9
ester <estercika83@gmail.com>
2024-10-23 12:53:40 -0500

    ejemplo

--- ejemplos-IDE/Ejemplo05/src/ejemplo05/Ejemplo05.java
+++ ejemplos-IDE/Ejemplo05/src/ejemplo05/Ejemplo05.java
@@ -27,17 +27,18 @@ public class Ejemplo05 {
         System.out.println(10 / (3.0 + 1));
         
         System.out.println("----------------");
+        // esta operacion es para hacer el calculo del residuo
         System.out.println(10 % 3 );
-        
+        // esta operacion es para sacar el residuo de la division
         System.out.println("----------------");
         System.out.println(Math.pow(10, 2));
-        
+        // esta operacion es para sacar el valor elevado al cuadrado
         System.out.println("----------------");
         System.out.println(Math.sqrt(16));
-        
+        // esta operacion es para sacar la raiz cuadrada 
         double m = Math.sqrt(16);
         System.out.println(m);
-        
+        // esta operacion es para imprimir el resultado
     }
 
 }
